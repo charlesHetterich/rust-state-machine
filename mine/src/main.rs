fca0ba09cc308c1b9909a88bb0ce7b137e6d0f61
chetterich <hetterich.charles@gmail.com>
2025-02-19 19:35:33 -0600

    through step 22

--- mine/src/main.rs
+++ mine/src/main.rs
@@ -1,6 +1,52 @@
 mod balances;
 mod system;
 
+pub struct Runtime {
+    system: system::Pallet,
+    balances: balances::Pallet,
+}
+
+impl Runtime {
+    fn new() -> Self {
+        let runtime = Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new(),
+        };
+
+        runtime
+    }
+}
+
+const NAMES: [&str; 10] = [
+    "Alice", "Bob", "Charlie", "Dave", "Eve", "Ferdie", "Grace", "Hank", "Ivy", "Judy",
+];
+
 fn main() {
-    println!("Hello, world!");
+    let mut runtime = Runtime::new();
+    runtime.balances.set_balance(&"Alice".to_string(), 100);
+
+    let mut idx = 1;
+    loop {
+        runtime.system.inc_block_number();
+        assert_eq!(runtime.system.block_number(), idx);
+
+        // alice sends 30 tokens to some account
+        runtime.system.inc_nonce(&"Alice".to_string());
+        let to = NAMES[idx as usize % 10];
+        let amount = 30;
+        let _ = runtime
+            .balances
+            .transfer(&"Alice".to_string(), &to.to_string(), amount)
+            .map_err(|e| eprintln!("{}", e));
+        println!(
+            "Block: {}, Alice: {}, {to}: {}",
+            runtime.system.block_number(),
+            runtime.balances.get_balance(&"Alice".to_string()),
+            runtime.balances.get_balance(&to.to_string())
+        );
+
+        // sleep 1 second
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        idx += 1;
+    }
 }
--- mine/src/system.rs
+++ mine/src/system.rs
@@ -17,4 +17,45 @@ impl Pallet {
             nonce: BTreeMap::new(),
         }
     }
+
+    /// Get current block number
+    pub fn block_number(&self) -> u32 {
+        self.block_number
+    }
+
+    /// Increment block number by one
+    pub fn inc_block_number(&mut self) {
+        self.block_number += 1;
+    }
+
+    // Increment an account's nonce
+    pub fn inc_nonce(&mut self, who: &String) {
+        let nonce = self.nonce.entry(who.clone()).or_insert(0);
+        *nonce += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init_system() {
+        let mut pallet = Pallet::new();
+
+        assert_eq!(pallet.block_number(), 0);
+        pallet.inc_block_number();
+        assert_eq!(pallet.block_number(), 1);
+    }
+
+    #[test]
+    fn init_nonce() {
+        let mut pallet = Pallet::new();
+
+        assert_eq!(pallet.nonce.get(&"Alice".to_string()), None);
+        pallet.inc_nonce(&"Alice".to_string());
+        assert_eq!(pallet.nonce.get(&"Alice".to_string()), Some(&1));
+        pallet.inc_nonce(&"Alice".to_string());
+        assert_eq!(pallet.nonce.get(&"Alice".to_string()), Some(&2));
+    }
 }
